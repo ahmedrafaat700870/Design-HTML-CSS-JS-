7730bb446a0909d055f7974146bf5c4d489da178
ahmedrafaat700870 <71382395+ahmedrafaat700870@users.noreply.github.com>
2022-01-09 09:49:18 -0800

    Add files via upload

---
+++ Colog_Page Section(Doctor)/js/script.js
@@ -0,0 +1,39 @@
+var thediv = document.getElementById("mydiv");
+var thehidd = document.getElementById("hidd");
+var thependivdesc = document.getElementById("opendiv-desc")
+var thependivdesc1 = document.getElementById("opendiv-desc1")
+var thependivdesc2 = document.getElementById("opendiv-desc2")
+var thependivdesc3 = document.getElementById("opendiv-desc3")
+var thependivdesc4 = document.getElementById("opendiv-desc4")
+var thependivdesc5 = document.getElementById("opendiv-desc5")
+var thependivdesc6 = document.getElementById("opendiv-desc6")
+var colorspan = document.getElementById("color-test")
+function opendiv(){
+    thediv.style.transform = "scale(1)"
+    thehidd.style.transform = "scale(0)"
+}
+function closediv (){
+    thediv.style.transform = "scale(0)"
+    thehidd.style.transform = "scale(1)"
+}
+function opendesc() {
+    thependivdesc.style.transform = "scale(1)"
+}
+function opendesc1() {
+    thependivdesc1.style.transform = "scale(1)"
+}
+function opendesc2() {
+    thependivdesc2.style.transform = "scale(1)"
+}
+function opendesc3() {
+    thependivdesc3.style.transform = "scale(1)"
+}
+function opendesc4() {
+    thependivdesc4.style.transform = "scale(1)"
+}
+function opendesc5() {
+    thependivdesc5.style.transform = "scale(1)"
+}
+function opendesc6() {
+    thependivdesc6.style.transform = "scale(1)"
+}
